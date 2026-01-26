148b34e26de32c3b8f11704efa1669f530c5f1df
stoneshik <strebilly@mail.ru>
2026-01-26 21:53:02 +0300

    до 7

--- src/main/java/lab/third/MainClass.java
+++ src/main/java/lab/third/MainClass.java
@@ -1,6 +1,27 @@
 package lab.third;
 
+/**
+* Главный класс приложения для демонстрации сборки JAR-файла.
+* <p>
+* Этот класс содержит точку входа в приложение и используется
+* в демонстрационных целях.
+* </p>
+*
+* @author Ilya
+* @version 1.0
+* @since 2025
+*/
 public class MainClass {
+    /**
+    * Точка входа в приложение.
+    * <p>
+    * Выводит информационное сообщение в консоль о предназначении
+    * данного JAR-файла.
+    * </p>
+    *
+    * @param args аргументы командной строки (не используются в данной реализации)
+    * @see System#out
+    */
     public static void main(String[] args) {
         System.out.println("This JAR is for build demonstration purposes.");
     }
